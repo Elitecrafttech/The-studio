740e8b65fd91a27ac1ba5ae1b06442e072f4291e
Elitecrafttech <Elitecrafttech@gmail.com>
2024-07-11 00:12:28 -1100

    tg

---
+++ index.js
@@ -0,0 +1,9 @@
+function openmenu() {
+    const tg = document.querySelector(".tg")
+    const close = document.querySelector(".close")
+    const open = document.querySelector(".open")
+    tg.classList.toggle('appear')
+    close.classList.toggle('pop')
+    open.classList.toggle('vanish')
+}
+
